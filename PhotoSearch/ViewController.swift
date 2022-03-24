6b60a56a4f19a1a212589d0ada0131c58d665311
Vitor Otero <vitorotero@icloud.com>
2022-03-24 17:36:47 +0000

    Update ViewController.swift
    Data from the API ( Console validate got data)

--- PhotoSearch/ViewController.swift
+++ PhotoSearch/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlString = "https://api.unsplash.com/search/photos/?page=1&query=office&client_id=W4D9pU4p1coeuTlrlAKvKwsHK-Y33uYiIyehxaQQ5o8"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchPhotos()
+        
     }
-
-
+    
+    func fetchPhotos() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            print("Got data!")
+        }
+        task.resume()
+    }
+    
+    
 }
 
